911dc4111080d59f582f951684d7f32d07cb1680
nickanthony-dgl <nicholas.anthony@digilens.com>
2023-06-21 16:08:59 -0500

    add test for exporting nlopt comparison

--- tests/test_nlopt.cpp
+++ tests/test_nlopt.cpp
@@ -142,6 +142,92 @@ TEST(Limbo_NLOpt, nlopt_no_grad_constraint)
     ASSERT_LE(std::abs(3. - best(1)), 0.000001);
 }
 
+// Loads a series of saved Bayesian models and evaluates the various NLOPT algorithms on them to see which one finds the highest maximum. In my testing the default of DIRECT_L_RAND always did best
+// template<nlopt::algorithm alg>
+// class NL: public opt::NLOptNoGrad<defaults::opt_nloptnograd, alg> {};
+//
+// TEST(Limbo_NLOpt, nlopt_comparison)
+// {
+//     struct MyParams
+//     {
+// 	    struct kernel_opt: defaults::kernel {};
+//
+// 	    struct kernel_squared_exp_ard : defaults::kernel_squared_exp_ard {};
+//
+// 	    struct acqui_ucb : defaults::acqui_ucb {};
+//     };
+//
+//     using Model = model::GP<kernel::SquaredExpARD<MyParams::kernel_opt, MyParams::kernel_squared_exp_ard>>;
+//     using Acqui = acqui::UCB<MyParams::acqui_ucb, Model>;
+//
+// 	std::filesystem::path root = R"(C:\Users\NicholasAnthony\source\repos\wt_gui\external\WaveTracer\testing\optimizerTests\resources\DIA_highD\optimizations\hp_10_irpropplus_dual_1eneg3_unbound)";
+//
+//     std::ofstream f(root / "data.csv");
+//     std::string header = "ISRES\tDIRECT_L_RAND\tORIG_DIRECT_L\tDIRECT\tDIRECT_L\tDIRECT_L_NOSCAL\tDIRECT_L_RAND_NOSCAL\tORIG_DIRECT\tESCH\tCRS2_LM\tAUG_LAG\n";
+//     std::cout << header;
+//     f << header;
+//
+//     for (auto const& entry : std::filesystem::directory_iterator(root))
+//     {
+//         if (!entry.is_directory() || !entry.path().filename().string().starts_with("modelArchive"))
+//             continue;
+//
+//         auto m = Model::load(serialize::TextArchive(entry.path().string()));
+//
+//         Acqui acqui(m);
+//         Eigen::VectorXd starting_point = tools::random_vector(m.dim_in(), true);
+//         auto parameterBounds = std::vector<std::pair<double, double>>(m.dim_in(), std::make_pair(0.0, 1.0));
+//
+//
+//         auto test = [&starting_point, &acqui, &parameterBounds](auto const& optimizer) -> double
+//         {
+//             double best;
+//             Eigen::VectorXd new_sample = optimizer.optimize(
+//                 [&](const Eigen::VectorXd& x, bool g) -> opt::eval_t { return acqui(x, g); },
+//                 starting_point,
+//                 parameterBounds,
+//                 &best);
+//             return best;
+//         };
+//
+//
+//         auto opt1 = NL<nlopt::GN_ISRES>::create(m.dim_in());
+//         auto opt2 = NL<nlopt::GN_DIRECT_L_RAND>::create(m.dim_in());
+//         auto opt3 = NL<nlopt::GN_ORIG_DIRECT_L>::create(m.dim_in());
+//         auto opt4 = NL<nlopt::GN_DIRECT>::create(m.dim_in());
+//         auto opt5 = NL<nlopt::GN_DIRECT_L>::create(m.dim_in());
+//         auto opt6 = NL<nlopt::GN_DIRECT_L_NOSCAL>::create(m.dim_in());
+//         auto opt7 = NL<nlopt::GN_DIRECT_L_RAND_NOSCAL>::create(m.dim_in());
+//         auto opt8 = NL<nlopt::GN_ORIG_DIRECT>::create(m.dim_in());
+//         auto opt9 = NL<nlopt::GN_ESCH>::create(m.dim_in());
+//         auto opt10 = NL<nlopt::GN_CRS2_LM>::create(m.dim_in());
+//         auto opt11 = NL<nlopt::AUGLAG>::create(m.dim_in());
+//
+//         auto tuple = std::make_tuple(opt1, opt2, opt3, opt4, opt5, opt6, opt7, opt8, opt9, opt10, opt11);
+//         std::vector<double> results;
+//         std::apply([&results, test](auto const& ... opts) 
+//             {
+// 				((results.push_back(test(opts))), ...);
+//             }, tuple);
+//
+//         for (int i=0; i<results.size(); i++)
+//         {
+//             std::cout << results.at(i);
+//             f << results.at(i);
+//             if (i == results.size() - 1)
+//             {
+//                 std::cout << "\n";
+//                 f << "\n";
+//             }
+//             else
+//             {
+//                 std::cout << "\t";
+//                 f << "\t";
+//             }
+//         }
+//     }
+// }
+
 //TODO this test crashed accessing a null value in NLOpt
 // TEST(Limbo_NLOpt, nlopt_grad_constraint)
 // {
